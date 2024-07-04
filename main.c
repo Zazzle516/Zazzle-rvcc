c99fd4c8bb380e176e105700ae5c5b180d0c66b8
Zazzle <2405677060@qq.com>
2024-07-04 21:52:35 +0800

    commit[4]: error info optimize

--- main.c
+++ main.c
@@ -14,6 +14,10 @@ typedef enum {
     TOKEN_OP                // 运算符
 } TokenKind;
 
+// 全局变量 记录输入流的起始位置(方便后续找到具体的错误位置)
+// 写在最上面 如果只是定义在 main() 上面的话 tokenize() 找不到
+static char* InputHEAD;
+
 typedef struct Token Token;
 struct Token {
     TokenKind token_kind;
@@ -26,6 +30,10 @@ struct Token {
 };
 
 // 有一个在看代码的时候也忽略掉了的 static 修饰 因为都只在当前文件使用 所有都可以用 static 修饰
+
+// 针对报错进行优化 errorHint() 和 errorAt() 是同级的错误信息展示
+// errorHint() 不去展示具体的语法错误未知 也就是不能用在具体的代码分析中 只可能能是一开始的参数数量错误
+// errorAt() 展示具体的语法错误位置
 
 static void errorHint(char* errorInfo, ...) {
     // 使用 <stdarg.h> 提供的变长参数
@@ -43,6 +51,48 @@ static void errorHint(char* errorInfo, ...) {
     exit(1);
 }
 
+// FMT VA 在 tokenize() 词法分析中定义的输出
+static void errorAt(char* place, char* FMT, va_list VA) {
+    // 打印出错输入(目前的输入都是一行)
+    fprintf(stderr, "%s\n", InputHEAD);
+
+    // 计算出错位置 并通过空格跳过
+    int err_place = place - InputHEAD;
+
+    // 通过 '%*s' 表示该输出字符串长度可变
+    fprintf(stderr, "%*s", err_place, "");
+
+    // 打印错误标志
+    fprintf(stderr, "^ ");
+
+    // 打印错误信息
+    fprintf(stderr, FMT, VA);
+    fprintf(stderr, "\n");
+
+    // 在这里统一清除变长参数
+    va_end(VA);
+
+    exit(1);
+}
+
+
+// 对错误信息的具体处理     针对 数字/符号 抽象出一层用来调用 errorAt()
+
+static void tokenErrorAt(Token* token, char* FMT, ...) {
+    va_list VA;
+    va_start(VA, FMT);
+
+    // 指明该 Token 的出错位置
+    errorAt(token->place, FMT, VA);
+}
+
+static void charErrorAt(char* place, char* FMT, ...) {
+    va_list VA;
+    va_start(VA, FMT);
+
+    errorAt(place, FMT, VA);
+}
+
 // 判断当前 Token 与指定 target 是否相同
 static bool equal(Token* input_token, char* target) {
     // 使用 memcmp 进行存储内容的比较   因为不清楚 Token 的存储类型 不能直接使用 '=='
@@ -52,7 +102,8 @@ static bool equal(Token* input_token, char* target) {
 // 指定 target 去跳过
 static Token* skip(Token* input_token, char* target) {
     if (!equal(input_token, target)) {
-        errorHint("expected: %s", target);
+        // errorHint("expected: %s", target);   针对 Token 进行错误优化
+        tokenErrorAt(input_token, "expected: %s", target);
     }
 
     // 不是根据 target 连续跳过 这个函数是用来跳过负号的(一次就够了 这么写会无法检测到语法错误)
@@ -64,7 +115,8 @@ static Token* skip(Token* input_token, char* target) {
 
 static int getNumber(Token* input_token) {
     if (input_token->token_kind != TOKEN_NUM) {
-        errorHint("expected number");
+        // errorHint("expected number");        同理根据 Token 优化错误信息
+        tokenErrorAt(input_token, "expected number");
     }
     // 把在 newToken() 中构造好的值取出
     return input_token->value;
@@ -84,8 +136,9 @@ static Token* newToken(TokenKind token_kind, char* start, char* end) {
 
 static Token* tokenize(char* input_ptr) {
     // 词法分析 根据链表的结构去调用 newToken() 构造
-    Token HEAD = {};            // 
-    Token* currToken = &HEAD;        // 指向 HEAD 类型为 Token 的指针 声明为 currToken
+    Token HEAD = {};                 // 声明 HEAD 为空
+    Token* currToken = &HEAD;        // 指向 HEAD 类型为 Token 的指针 声明为 currToken 注意是指针哦 通过 '->' 调用
+    char* P = InputHEAD;
 
     while(*input_ptr) {
         // 对空格没有使用 while 因为如果在里面写了 continue 的话 while 实际上就没有意义了
@@ -121,13 +174,16 @@ static Token* tokenize(char* input_ptr) {
             continue;
         }
 
-        errorHint("Wrong Syntax: %c", *input_ptr);
+        charErrorAt(input_ptr, "Wrong Syntax: %c", *input_ptr);
     }
 
     // 添加终结符
     currToken->next = newToken(TOKEN_EOF, input_ptr, input_ptr);
+
+    // 对直接存在的结构体调用 "成员变量"
     return HEAD.next;
 }
+
 
 int main(int argc, char* argv[]) {
     // 首先是对异常状态的判断
@@ -142,6 +198,7 @@ int main(int argc, char* argv[]) {
     
     // 得到指向输入流的指针
     char* input_ptr = argv[1];
+    InputHEAD = argv[1];
     
     // 把输入流进行标签化 得到 TokenStream 链表     此时没有空格
     Token* input_token = tokenize(input_ptr);
@@ -166,7 +223,7 @@ int main(int argc, char* argv[]) {
         input_token = input_token->next;
     }
 
-    // 考虑到终结情况 比如 1-1-1- 转化为 1-1-1-0    虽然功能上没问题 但是应该报错 Warning
+    // 考虑到终结情况 比如 1-1-1- 转化为 1-1-1-0    (虽然功能上没问题 但是应该报错 Warning
     printf("  ret\n");
     return 0;
 }
